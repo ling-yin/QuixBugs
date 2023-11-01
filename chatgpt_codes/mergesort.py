62fb819ec1cbe7f84b1d17842245fb1b3f207cb9
ling-yin <64789716+ling-yin@users.noreply.github.com>
2023-11-02 00:34:21 +0800

    chatGPT corrected ver

---
+++ chatgpt_codes/mergesort.py
@@ -0,0 +1,30 @@
+The bug in the code is that the `mergesort` function is missing a base case to handle the scenario when the input array has only one element. 
+
+Here is the corrected code:
+
+```python
+def mergesort(arr):
+    def merge(left, right):
+        result = []
+        i = 0
+        j = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        result.extend(left[i:] or right[j:])
+        return result
+
+    if len(arr) <= 1:  # Added base case for array with one or zero elements
+        return arr
+    else:
+        middle = len(arr) // 2
+        left = mergesort(arr[:middle])
+        right = mergesort(arr[middle:])
+        return merge(left, right)
+```
+
+This code will handle the case when the input array has only one element, preventing an infinite recursion.
---
+++ chatgpt_codes/possible_change.py
@@ -0,0 +1,10 @@
+```python
+def possible_change(coins, total):
+    if total == 0:
+        return 1
+    if total < 0:
+        return 0
+
+    first, *rest = coins
+    return possible_change(coins, total - first) + possible_change(rest, total - first)
+```
